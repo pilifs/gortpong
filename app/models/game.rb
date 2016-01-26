579f5401c0583d9c8f0dd969ed9cea39150036af
Filip Stankovic <filips@gmail.com>
2016-01-26 16:58:34 -0500

    Add dummy players and game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
-  belongs_to :winner, :class_name 'Player', :foreign_key => 'winner_id'
-  belongs_to :loser, :class_name 'Player', :foreign_key => 'loser_id'
+  belongs_to :winner, :class_name => 'Player', :foreign_key => 'winner_id'
+  belongs_to :loser, :class_name => 'Player', :foreign_key => 'loser_id'
 end
