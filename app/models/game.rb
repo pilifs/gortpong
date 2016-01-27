0c4c91bd55d29b45fdc3573964758def4b2a36ff
Filip Stankovic <filips@gmail.com>
2016-01-26 23:08:13 -0500

    Cleanup, redo initial values properly

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,19 +2,12 @@ class Game < ActiveRecord::Base
   belongs_to :winner, :class_name => 'Player', :foreign_key => 'winner_id'
   belongs_to :loser, :class_name => 'Player', :foreign_key => 'loser_id'
 
-  after_save :update_players
+  after_create :update_players
 
   protected
   def update_players
-    # byebug
     self.winner.increment_wins
-    # byebug
-    # self.winner.save!
-    # byebug
     self.loser.increment_losses
-    # byebug
-    # self.loser.save!
-    # byebug
   end
 
 end
