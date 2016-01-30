9c2585e121b1ea72a850eb213f189081ad63718b
Chris Staikos <cstaikos@gmail.com>
2016-01-29 20:22:45 -0500

    Add form for creating new game, and link from rankings list to log game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,25 @@ class GamesController < ApplicationController
     @games = Game.all
   end
 
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to players_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:winner_slack, :winner_score, :loser_slack, :loser_score)
+  end
+
+
 end
