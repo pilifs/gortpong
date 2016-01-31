f0192e0adf72bb70d658585c3330d78a3f2d4261
Filip Stankovic <filips@gmail.com>
2016-01-31 10:13:13 -0500

    Make game display mirror player on index

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
 
   def index
     @players = Player.includes(:rating).order('ratings.rating DESC')
-    @games = Game.all.order(created_at: :desc).limit(10)
+    @games = Game.all.order(created_at: :desc).limit(@players.count)
   end
 
   def show
