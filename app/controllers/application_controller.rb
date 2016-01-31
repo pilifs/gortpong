c1fefdd453b8e9dca772ebbf2174ecf525306449
Filip Stankovic <filips@gmail.com>
2016-01-31 09:43:18 -0500

    Fix logic for displaying at a glance boxes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :at_a_glance
+
+  def at_a_glance
+    @plus_minus = Player.all.order(plus_minus: :desc).limit(3)
+  end
+
 end
