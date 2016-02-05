5f6a150e9c2b07a8fa3cec8cda5d1b9fbdf1b99d
Chris Staikos <cstaikos@gmail.com>
2016-02-04 23:09:02 -0500

    Add popover text, and js triggers for stat boxes

---
+++ app/assets/javascripts/_ataglance.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  // Attach popovers to each stat box - the content is stored in divs immediately
+  // after the stat box (siblings on same level in DOM).
+  $(".statbox-wrapper").each(function(index, element) {
+    $(element).popover({
+      trigger: 'hover',
+      container: '.at-a-glance-outer',
+      placement: 'bottom',
+      content: function() {
+        return $(element).nextAll(".stat-help:first").text();
+      }
+    });
+
+  });
+
+});
