8efc9131b71e6d3c29500bbb4e897c77891e5f21
Filip Stankovic <filips@gmail.com>
2016-02-07 09:33:44 -0500

    Update all dbs to postgres and update seed data

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160206171630) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer  "winner_score"
     t.integer  "loser_score"
