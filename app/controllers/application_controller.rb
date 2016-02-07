972a96f699b6a158d15c17399413a2ca8c6d644b
Filip Stankovic <filips@gmail.com>
2016-02-07 12:57:37 -0500

    Add current page highlight to nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :at_a_glance
 
   # TODO: This should show if there is a tie for 3rd place by putting (ie. Tie: 3 players) instead of showing the player with highest id in tie.
+  # TODO: The sort queries should probably be a seperate column for performance reasons?
   def at_a_glance
     @ag_plus_minus = Player.all.order(plus_minus: :desc).limit(5)
     @ag_toughest_opponents = Player.includes(:rating).order("ratings.avg_opp_rating DESC").limit(5)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cp(path)
+    "current-link" if current_page?(path)
+  end
+
+  def btn_path(path)
+    "current-btn" if current_page?(path)
+  end
 end
