e2c88180dfa1eca0c8ffa763fb8c2c1c024fe1b8
Filip Stankovic <filips@gmail.com>
2016-03-29 17:14:21 -0400

    Create factory to generate rating object

---
+++ spec/factories/ratings.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :good_rating do
+    rating '1600'
+    highest_ever '1550'
+    avg_opp_rating '1500'
+    avg_opp_rating_win '1490'
+    avg_opp_rating_loss '1510'
+  end
+end
