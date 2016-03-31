55b276d2bbab16e9feb138b9c5ad5d813f2c0701
Filip Stankovic <filips@gmail.com>
2016-03-31 17:39:05 -0400

    Add more pending tests, make one pass

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -17,12 +17,18 @@ describe Player do
   it "must have a unique slack handle"
 
   describe '#win_percentage' do
-    it "returns n/a with no games played"
+    it "returns n/a with no games played" do
+      never_played = build(:avg_player, games_played: 0)
+      expect(never_played.win_percentage).to eq('n/a')
+    end
     it "returns 100 with only wins and no losses"
     it "returns 0 with only losses and no wins"
     it "rounds to 2 decimal places"
     it "accurately returns a percentage of wins / total games"
   end
 
+  describe '#avg_points' do
+
+  end
 
 end
