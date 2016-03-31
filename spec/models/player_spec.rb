1d3d211d9e2a6e6ce153462475c4cb6f1e341df6
Filip Stankovic <filips@gmail.com>
2016-03-31 16:48:55 -0400

    Update test syntax (should deprecated), add some pending tests

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -5,9 +5,18 @@ require 'factory_girl'
 describe Player do
   before(:each) do
     @good_player = build(:good_player)
+    @avg_player = build(:avg_player)
+    @bad_player = build(:bad_player)
   end
 
   it "has a valid factory" do
-    @good_player.should be_valid
+    expect(@good_player).to be_valid
   end
+
+  it "is invalid without a display name"
+  it "is invalid without a slack handle"
+  it "must have a unique display name"
+  it "must have a unique slack handle"
+  
+
 end
