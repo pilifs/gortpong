3bb3ca2733eb83b06f15b58bb932c9daac9f0c3f
Filip Stankovic <filips@gmail.com>
2016-04-01 14:26:19 -0400

    Add context to some tests. Make tests pass

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -11,26 +11,34 @@ describe Player do
     expect(@good_player).to be_valid
   end
 
-  it "is invalid without a display name"
-  it "is invalid without a slack handle"
-  it "must have a unique display name"
-  it "must have a unique slack handle"
+  context "when creating a new player" do
+    it "is invalid without a display name"
+    it "is invalid without a slack handle"
+    it "must have a unique display name"
+    it "must have a unique slack handle"
+  end
 
   describe '#win_percentage' do
-    it "multiplies basic win percentage by 100" do
-      wins = 3
-      losses = 3
-      games_played = wins + losses
-      fifty_percent = build(:avg_player, wins: wins, losses: losses, games_played: games_played)
-      expect(fifty_percent.win_percentage).to eq(wins / games_played.to_f * 100)
+    context "with equal wins and losses" do
+      it "should return 50.0" do
+        wins = 3
+        losses = 3
+        games_played = wins + losses
+        fifty_percent = build(:avg_player, wins: wins, losses: losses, games_played: games_played)
+        expect(fifty_percent.win_percentage).to eq(wins / games_played.to_f * 100)
+      end
     end
-    it "returns n/a with no games played" do
-      never_played = build(:avg_player, games_played: 0)
-      expect(never_played.win_percentage).to eq('n/a')
+    context "with no games played" do
+      it "returns n/a with no games played" do
+        never_played = build(:avg_player, games_played: 0)
+        expect(never_played.win_percentage).to eq('n/a')
+      end
     end
-    it "returns 100 with only wins and no losses" do
-      all_wins = build(:good_player, losses: 0)
-      expect(all_wins.win_percentage).to eq(100)
+    context "with only wins and no losses" do
+      it "returns 100" do
+        all_wins = build(:good_player, losses: 0)
+        expect(all_wins.win_percentage).to eq(100)
+      end
     end
     it "returns 0 with only losses and no wins" do
       no_wins = build(:bad_player, wins: 0)
@@ -43,7 +51,12 @@ describe Player do
   end
 
   describe '#avg_points' do
-
+    context "with no games played" do
+      it "should return 0" do
+        no_games = build(:bad_player, games_played: 0)
+        expect(no_games.avg_points).to eq(0)
+      end
+    end
   end
 
 end
