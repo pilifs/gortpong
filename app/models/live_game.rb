8c059538b5d4ffb4ace90438127f8ef8b277fbdb
Mike Bowman <mbowman2@gmail.com>
2016-03-31 23:07:23 -0400

    only one live game can exist, only registered users ccan join

--- app/models/live_game.rb
+++ app/models/live_game.rb
@@ -1,3 +1,14 @@
 class LiveGame < ActiveRecord::Base
   validates_presence_of :player_one_slack, :player_two_slack, :player_one_score, :player_two_score
+  validates :player_one_score, :player_two_score, numericality: true
+  validate :check_player_exists
+  validate :check_only_one_game_exists
+
+  def check_only_one_game_exists
+    errors.add(:base, "Only one live game can exist at a time.") if LiveGame.all.count != 0
+  end
+
+  def check_player_exists
+    errors.add(:base, "Players must have a valid slack handle.") unless Player.find_by(slack_handle: self.player_one_slack) && Player.find_by(slack_handle: self.player_two_slack)
+  end
 end
