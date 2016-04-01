3521d7a1558684dcda0af375f1b5d24c01b1340d
Mike Bowman <mbowman2@gmail.com>
2016-03-31 20:32:18 -0400

    create live game forms, validations to model, show view kinda works

--- app/controllers/live_games_controller.rb
+++ app/controllers/live_games_controller.rb
@@ -1,9 +1,33 @@
 class LiveGamesController < ApplicationController
 
   def new
+    @live_game = LiveGame.new
+  end
+
+  def create
+    @live_game = LiveGame.create(live_game_params)
+    @live_game.player_one_score = 0
+    @live_game.player_two_score = 0
+
+    if @live_game.save
+      redirect_to live_game_path(@live_game)
+    else
+      flash[:notice] = "Something went wrong. Please try again."
+      render :new
+    end
   end
 
   def show
+    @live_game = LiveGame.find(params[:id])
+    @player_one_slack = @live_game.player_one_slack
+    @player_two_slack = @live_game.player_two_slack
+    @player_one_score = @live_game.player_one_score
+    @player_two_score = @live_game.player_two_score
   end
-  
+
+  private
+  def live_game_params
+    params.require(:live_game).permit(:player_one_slack, :player_two_slack, :player_one_score, :player_two_score)
+  end
+
 end
--- app/models/live_game.rb
+++ app/models/live_game.rb
@@ -1,2 +1,3 @@
 class LiveGame < ActiveRecord::Base
+  validates_presence_of :player_one_slack, :player_two_slack, :player_one_score, :player_two_score
 end
