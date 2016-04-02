ed992da9771466a64ed24c00126fb692e40731ac
Filip Stankovic <filips@gmail.com>
2016-04-02 19:27:58 -0400

    Add pending tests for live game model

--- spec/factories/live_games.rb
+++ spec/factories/live_games.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :live_game do
-    
+    player_one_slack filips
+    player_two_slack nil
+    in_progress true
   end
 end
--- spec/models/live_game_spec.rb
+++ spec/models/live_game_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe LiveGame, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#reset_game' do
+    it 'sets score to 0'
+    it 'sets in_progress to false'
+    it 'sets slack handles to nil'
+  end
+
+  describe '#display_name' do
+    context 'when given a player slack handle' do
+      it 'returns the associated display name'
+    end
+    context 'when slack handle is nil' do
+      it 'returns awaiting game text'
+    end
+  end
+
+
 end
