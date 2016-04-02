90d8785e2775d2923c649bee984feb8956bb5e1a
Filip Stankovic <filips@gmail.com>
2016-04-02 14:03:18 -0400

    Comment out outdated validations. May revisit

--- app/models/live_game.rb
+++ app/models/live_game.rb
@@ -1,9 +1,9 @@
 class LiveGame < ActiveRecord::Base
-  validates_presence_of :player_one_slack, :player_two_slack, :player_one_score, :player_two_score
-  validates :player_one_score, :player_two_score, numericality: true
-  validate :check_player_exists
-  validate :table_name, presence: true
-  validate :check_only_one_game_exists, on: [:create]
+  validates_presence_of :table_name
+  # validates_presence_of :player_one_slack, :player_two_slack, :player_one_score, :player_two_score
+  # validates :player_one_score, :player_two_score, numericality: true
+  # validate :check_player_exists
+  # validate :check_only_one_game_exists, on: [:create]
 
   after_initialize :set_defaults
 
@@ -13,11 +13,11 @@ class LiveGame < ActiveRecord::Base
     self.in_progress ||= false
   end
 
-  def check_only_one_game_exists
-    errors.add(:base, "Only one live game can exist at a time.") if LiveGame.all.count != 0
-  end
-
-  def check_player_exists
-    errors.add(:base, "Players must have a valid slack handle.") unless Player.find_by(slack_handle: self.player_one_slack) && Player.find_by(slack_handle: self.player_two_slack)
-  end
+  # def check_only_one_game_exists
+  #   errors.add(:base, "Only one live game can exist at a time.") if LiveGame.all.count != 0
+  # end
+  #
+  # def check_player_exists
+  #   errors.add(:base, "Players must have a valid slack handle.") unless Player.find_by(slack_handle: self.player_one_slack) && Player.find_by(slack_handle: self.player_two_slack)
+  # end
 end
