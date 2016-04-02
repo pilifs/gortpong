3f8ce17aeca7c4a96072fd88e9054fea470295c5
Filip Stankovic <filips@gmail.com>
2016-04-02 14:04:28 -0400

    Adjust create action logic to create a persistent live game instance (ie. live game == ping pong table)

--- app/controllers/live_games_controller.rb
+++ app/controllers/live_games_controller.rb
@@ -1,7 +1,15 @@
 class LiveGamesController < ApplicationController
 
   def index
-    @live_game = LiveGame.first
+    @live_games = LiveGame.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def show
+    @live_game = LiveGame.find(params[:id])
     respond_to do |format|
       format.html
       format.json
@@ -13,9 +21,7 @@ class LiveGamesController < ApplicationController
   end
 
   def create
-    @live_game = LiveGame.create(live_game_params)
-    @live_game.player_one_score = 0
-    @live_game.player_two_score = 0
+    @live_game = LiveGame.create(live_game_create_params)
 
     if @live_game.save
       redirect_to live_game_path(@live_game)
@@ -23,6 +29,10 @@ class LiveGamesController < ApplicationController
       flash[:notice] = "Something went wrong. Please try again."
       render :new
     end
+  end
+
+  def edit
+    @live_game = LiveGame.find(params[:id])
   end
 
   def update
@@ -41,6 +51,7 @@ class LiveGamesController < ApplicationController
 
   def destroy
     @live_game = LiveGame.find(params[:id])
+    @live_game.reset_game
 
     if @live_game.destroy
       redirect_to root_path
@@ -50,8 +61,22 @@ class LiveGamesController < ApplicationController
   end
 
   private
+  def live_game_create_params
+    params.require(:live_game).permit(:table_name)
+  end
+
   def live_game_params
     params.require(:live_game).permit(:player_one_slack, :player_two_slack, :player_one_score, :player_two_score)
+  end
+
+  def start_game
+    live_game.in_progress = true
+  end
+
+  def reset_game
+    live_game.player_one_score = 0
+    live_game.player_two_score = 0
+    live_game.in_progress = false
   end
 
 end
