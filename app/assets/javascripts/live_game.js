74beae987338fb8f34642ab3ee9d8e8a4dc6ee97
Mike Bowman <mbowman2@gmail.com>
2016-04-02 12:41:31 -0400

    add links to create new live game from index page

--- app/assets/javascripts/live_game.js
+++ app/assets/javascripts/live_game.js
@@ -1,11 +1,11 @@
 $(document).on('ready page:load', function() {
-  var liveGameInfo = {
-    "$('#player-one-slack').html()" : $('#player-one-score').html(),
-    "$('#player-two-slack').html()" : $('#player-two-score').html()
-  }
   $(document).on('click', '#submit-live-game-button', function(e) {
-    e.preventDefault();
+    var liveGameInfo = new Object();
+    liveGameInfo.playerOneSlack = $('#player-one-slack').html();
+    liveGameInfo.playerTwoSlack = $('#player-two-slack').html();
+    liveGameInfo.playerOneScore = $('#player-one-score').html();
+    liveGameInfo.playerTwoScore = $('#player-two-score').html();
 
-    alert("WOOHOO")
+    console.log(liveGameInfo)
   })
 })
