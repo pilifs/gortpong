22ed7f25870bfcdeef8918105ab8174b2e84bbab
Filip Stankovic <filips@gmail.com>
2016-04-02 23:26:36 -0400

    Minor changes to rating factory

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
-  factory :good_rating do
+  factory :good_rating, class: Rating do
     rating '1600'
     highest_ever '1550'
     avg_opp_rating '1500'
     avg_opp_rating_win '1490'
     avg_opp_rating_loss '1510'
+  end
+
+  factory :bad_rating, class: Rating do
+    rating '1300'
+  end
+
+  factory :rating do
   end
 end
